1912880cb61a575aa1686c429bfa7e2e5a5b3936
chinnawat.w <chinnawat@orcsoft.co.th>
2019-04-10 09:36:31 +0700

    Initial Commit

---
+++ customer/customer.go
@@ -0,0 +1,221 @@
+package customer
+
+import (
+	"database/sql"
+	"fmt"
+
+	"../model"
+
+	// pq is a postgres driver
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "changeme"
+	dbname   = "profile"
+)
+
+func connectDB() (db *sql.DB, err error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+// CheckDuplicateToken is a function to check duplicate token before create new member detail
+func CheckDuplicateToken(token string) (err error) {
+	db, err := connectDB()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	_, err = db.Query(`SELECT customer.token FROM users.customers as customer where customer.token='$1';`, token)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// InsertCustomer is used to insert new detail to db
+func InsertCustomer(token string, customer *model.Customer) (err error) {
+	db, err := connectDB()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	sqlStatement := `INSERT INTO users.customers (token, name, age, email, phone) VALUES ($1, $2, $3, $4, $5);`
+
+	_, err = db.Exec(sqlStatement, token, customer.Name, customer.Age, customer.Email, customer.Phone)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func mapCustomerDetail(row *sql.Row) (customer *model.Customer, err error) {
+	var name, email, phone string
+	var age int
+
+	switch err := row.Scan(&name, &age, &email, &phone); err {
+	case sql.ErrNoRows:
+		return nil, sql.ErrNoRows
+	case nil:
+		customer = model.NewCustomer(name, age, email, phone)
+		break
+	default:
+		return nil, err
+	}
+
+	return customer, nil
+}
+
+// FindCustomerDetailByToken is used to find customer detail by token
+func FindCustomerDetailByToken(token string) (customer *model.Customer, err error) {
+	db, err := connectDB()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	row := db.QueryRow(`SELECT customer.name, customer.age, customer.email, customer.phone FROM users.customers as customer where customer.token=$1`, token)
+	customer, err = mapCustomerDetail(row)
+	if err != nil {
+		return nil, err
+	}
+
+	return customer, nil
+}
+
+func mapAllCustomersDetail(rows *sql.Rows) (customers []*model.Customer, err error) {
+	for rows.Next() {
+		var name, email, phone string
+		var age int
+		err = rows.Scan(&name, &age, &email, &phone)
+		if err != nil {
+			return nil, err
+		}
+		customer := model.NewCustomer(name, age, email, phone)
+		customers = append(customers, customer)
+	}
+
+	return customers, nil
+}
+
+// FindAllCustomerDetail is used to show all customers detail
+func FindAllCustomerDetail() (customers []*model.Customer, err error) {
+	db, err := connectDB()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	rows, err := db.Query(`SELECT customer.name, customer.age, customer.email, customer.phone FROM users.customers as customer`)
+	if err != nil {
+		return nil, err
+	}
+
+	customers, err = mapAllCustomersDetail(rows)
+	if err != nil {
+		return nil, err
+	}
+
+	return customers, nil
+}
+
+// UpdateCustomerDetail is used for update Customer data
+func UpdateCustomerDetail(token string, customer *model.Customer) (err error) {
+	db, err := connectDB()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	sqlStatement := `UPDATE users.customers SET name=$2, age=$3, email=$4, phone=$5 WHERE token=$1;`
+	_, err = db.Exec(sqlStatement, token, customer.Name, customer.Age, customer.Email, customer.Phone)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func mapCustomerID(row *sql.Row) (id uint64, err error) {
+
+	switch err := row.Scan(&id); err {
+	case sql.ErrNoRows:
+		return 0, sql.ErrNoRows
+	case nil:
+		break
+	default:
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func findCustomerIDByToken(token string) (id uint64, err error) {
+	db, err := connectDB()
+	if err != nil {
+		return 0, err
+	}
+	defer db.Close()
+
+	row := db.QueryRow(`SELECT customer.id FROM users.customers as customer where customer.token=$1`, token)
+	id, err = mapCustomerID(row)
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+// UpdateCustomerToken is used for update customer token after update customer detail
+func UpdateCustomerToken(existToken string, newToken string) (err error) {
+	id, err := findCustomerIDByToken(existToken)
+	if err != nil {
+		return err
+	}
+
+	db, err := connectDB()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	sqlStatement := `UPDATE users.customers SET token=$2 WHERE id=$1;`
+	_, err = db.Exec(sqlStatement, id, newToken)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DeleteCustomerByToken is used for delete customer
+func DeleteCustomerByToken(token string) (err error) {
+	db, err := connectDB()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	sqlStatement := `DELETE FROM users.customers as customer where customer.token=$1;`
+	_, err = db.Exec(sqlStatement, token)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ service/service.go
@@ -0,0 +1,155 @@
+package service
+
+import (
+	"crypto/rand"
+	"fmt"
+	"net/http"
+
+	"../customer"
+	"../model"
+	"../storage"
+
+	"github.com/labstack/echo"
+)
+
+func tokenGenerator() string {
+	b := make([]byte, 32)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)
+}
+
+func prepareToken() string {
+	for {
+		token := tokenGenerator()
+		checkToken := customer.CheckDuplicateToken(token)
+		if checkToken != nil {
+			return token
+		}
+	}
+}
+
+func prepareMessageResponse(statusCode string, statusMessage string) *model.MessageResponse {
+	return model.NewMessageResponse(statusCode, statusMessage)
+}
+
+func prepareMessageInsertResponse(statusCode string, statusMessage string, token string) *model.MessageInsertResponse {
+	return model.NewMessageInsertResponse(statusCode, statusMessage, token)
+}
+
+func prepareMessageFindResponse(statusCode string, statusMessage string, detail *model.Customer) *model.MessageCustomerResponse {
+	return model.NewMessageCustomerResponse(statusCode, statusMessage, detail)
+}
+
+func prepareMessageAllListResponse(statusCode string, statusMessage string, customers []*model.Customer) *model.MessageListCustomersResponse {
+	return model.NewMessageListCustomersResponse(statusCode, statusMessage, customers)
+}
+
+func prepareMessageUpdateCustomerResponse(statusCode string, statusMessage string, token string) *model.MessageUpdateCustomerResponse {
+	return model.NewMessageUpdateCustomerResponse(statusCode, statusMessage, token)
+}
+
+func callInsertCustomer(cust *model.Customer) (token string, err error) {
+	token = prepareToken()
+	detail := model.NewCustomer(cust.Name, cust.Age, cust.Email, cust.Phone)
+	if err = customer.InsertCustomer(token, detail); err != nil {
+		return "", err
+	}
+	return token, nil
+}
+
+// InsertCustomer is used to save new customer detail
+func InsertCustomer(c echo.Context) (err error) {
+	cust := new(model.Customer)
+	if err = c.Bind(&cust); err != nil {
+		return c.JSON(http.StatusOK, prepareMessageResponse("1003", err.Error()))
+	}
+
+	token, err := callInsertCustomer(cust)
+	if err != nil {
+		return c.JSON(http.StatusOK, prepareMessageResponse("1003", err.Error()))
+	}
+
+	return c.JSON(http.StatusOK, prepareMessageInsertResponse("0", "success", token))
+}
+
+// FindCustomer is used to find customer detail by token
+func FindCustomer(c echo.Context) (err error) {
+	msgCustomerRequest := new(model.MessageCustomerRequest)
+	if err = c.Bind(&msgCustomerRequest); err != nil {
+		return c.JSON(http.StatusOK, prepareMessageResponse("1003", err.Error()))
+	}
+
+	storage.SetUserToken(msgCustomerRequest.Token)
+	detail, err := customer.FindCustomerDetailByToken(storage.GetUserToken())
+	if err != nil {
+		return c.JSON(http.StatusOK, prepareMessageResponse("1003", err.Error()))
+	}
+
+	return c.JSON(http.StatusOK, prepareMessageFindResponse("0", "success", detail))
+}
+
+// ListCustomer is used show all customer detail
+func ListCustomer(c echo.Context) (err error) {
+	customers, err := customer.FindAllCustomerDetail()
+	if err != nil {
+		return c.JSON(http.StatusOK, prepareMessageResponse("1003", err.Error()))
+	}
+
+	return c.JSON(http.StatusOK, prepareMessageAllListResponse("0", "success", customers))
+}
+
+func validateTokenForUpdate(token string) bool {
+	return token == storage.GetUserToken()
+}
+
+func callUpdateCustomer(request *model.MessageUpdateCustomerRequest, c echo.Context) (err error) {
+	err = customer.UpdateCustomerDetail(request.Token, request.Customer)
+	if err != nil {
+		return c.JSON(http.StatusOK, prepareMessageResponse("1003", err.Error()))
+	}
+
+	newToken := prepareToken()
+	err = customer.UpdateCustomerToken(request.Token, newToken)
+	if err != nil {
+		return c.JSON(http.StatusOK, prepareMessageResponse("1003", err.Error()))
+	}
+
+	storage.SetUserToken("")
+	return c.JSON(http.StatusOK, prepareMessageUpdateCustomerResponse("0", "success", newToken))
+}
+
+// UpdateCustomer is used for update customer detail
+func UpdateCustomer(c echo.Context) (err error) {
+	msgUpdateCustomerRequest := new(model.MessageUpdateCustomerRequest)
+	if err = c.Bind(&msgUpdateCustomerRequest); err != nil {
+		return c.JSON(http.StatusOK, prepareMessageResponse("1003", err.Error()))
+	}
+
+	if !validateTokenForUpdate(msgUpdateCustomerRequest.Token) {
+		return c.JSON(http.StatusOK, prepareMessageResponse("1003", "Invalid Token"))
+	}
+
+	return callUpdateCustomer(msgUpdateCustomerRequest, c)
+}
+
+func callDeleteCustomer(token string, c echo.Context) (err error) {
+	if err = customer.DeleteCustomerByToken(token); err != nil {
+		return c.JSON(http.StatusOK, prepareMessageResponse("1003", err.Error()))
+	}
+
+	return c.JSON(http.StatusOK, prepareMessageResponse("0", "success"))
+}
+
+// DeleteCustomer is used for delete customer
+func DeleteCustomer(c echo.Context) (err error) {
+	msgCustomerRequest := new(model.MessageCustomerRequest)
+	if err = c.Bind(&msgCustomerRequest); err != nil {
+		return c.JSON(http.StatusOK, prepareMessageResponse("1003", err.Error()))
+	}
+
+	if !validateTokenForUpdate(msgCustomerRequest.Token) {
+		return c.JSON(http.StatusOK, prepareMessageResponse("1003", "Invalid Token"))
+	}
+
+	return callDeleteCustomer(msgCustomerRequest.Token, c)
+}
