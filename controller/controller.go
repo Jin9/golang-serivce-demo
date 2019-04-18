080ccf97732582af87d4fc7147ba1b31448a6409
chinnawat.w <chinnawat@orcsoft.co.th>
2019-04-18 13:47:27 +0700

    add allow Origins

--- controller/controller.go
+++ controller/controller.go
@@ -1,14 +1,22 @@
 package controller
 
 import (
+	"net/http"
+
 	"gitlab.com/chinnawat.w/golang-service-demo/service"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 // Init used for annouce route path of api
 func Init() *echo.Echo {
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://172.20.10.10:12001"},
+		AllowMethods: []string{http.MethodGet, http.MethodPatch, http.MethodPost},
+	}))
 
 	// routing
 	e.POST("/customer", service.InsertCustomer)
